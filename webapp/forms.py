0c33f06a0d86bed61ddb4f26d89439f850a705db
Adam <adam.nemmish213@gmail.com>
2024-01-13 18:14:01 +0600

    Выполнил 2 этап

---
+++ webapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from webapp.models import Product, Review
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'category', 'image']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model =Review
+        fields = ['author', 'product', 'text', 'rating', 'moderate']
+        widgets = {'rating': forms.CheckboxSelectMultiple}
+
+
+class ProductUserForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['users']
+        widgets = {'users': forms.CheckboxSelectMultiple}
--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
+from webapp.views.product_views import (ProductListView, ProductDetailView, ProductCreateView,
+                                        ProductUpdateView, ProductDeleteView)
+from webapp.views.revew_views import ReviewCreateView
+
+app_name = 'webapp'
 
 urlpatterns = [
-
+    path('', ProductListView.as_view(), name='home'),
+    path('product/', RedirectView.as_view(pattern_name='webapp:home')),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='detail_product'),
+    path('product/add/', ProductCreateView.as_view(), name='create_product'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='update_product'),
+    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='delete_product'),
+    path('review/add/', ReviewCreateView.as_view(), name='create_review'),
 ]
