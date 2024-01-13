5a82c5fdcf9468b7bfc0d21970f6744fab5c8162
Adam <adam.nemmish213@gmail.com>
2024-01-13 13:05:29 +0600

    Выполнил 1 задание, написал модели накатил меграции и применил, зарегал в админке, так же на будущее сделал новый прект accounts

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    category = models.CharField(max_length=35, verbose_name='Категория', choices=[('Dark chocolate', 'Черный'),('Milk chocolate', 'Молочный'), ('White chocolate', 'Белый')])
+    description = models.TextField(max_length=200, null=True, blank=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='product_images', verbose_name='Картинка', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Автор')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name='Товар')
+    text = models.TextField(verbose_name='Текст', max_length=200)
+    rating = models.PositiveIntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')], verbose_name='Оценка')
+    moderate = models.BooleanField(default=False, verbose_name='Промодерирован')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return f'{self.author.name} {self.product.name}'
